0290bd705b99f16e717197510ca48ea976dfd5e2
hakansal <hakansal3149@yandex.com>
2024-09-21 23:53:41 +0300

    c

---
+++ src/components/kartlar/kartlar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Kart=()=>{
+
+    return <div>
+        <div className="main">
+
+            <div className="rows">
+
+            </div>
+
+        </div>
+
+
+    </div>
+}
+
+export default Kart;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,38 +1,21 @@
 import React from "react";
-
-const Navbar=()=>{
-    return<div>
+import { Link } from "react-router-dom";
+import  "./navbar.css";
+const Navbar = () => {
+  return <div>
+    <div>
+      <nav className="navbar">
         <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Bahçeken avm
+          <h1>logo</h1>
+        </div>
+        <div className="buttons">
+           <Link to="/konum">Konum</Link> 
+          <Link to="/ürünler">Ürünler</Link>
+          <Link to="/iletişim">İletişim</Link>
 
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/ürünler">Ürünler</a>
-        </li>
-       
-       
-      </ul>
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/iletişim">iletişim</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/konum">Konum</a>
-        </li>
-       
-      </ul>
-    
+        </div>
+      </nav>
     </div>
   </div>
-</nav>
-        </div>
-    </div>
 };
 export default Navbar;
